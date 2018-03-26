cc389151c41353b69ea68a010a63e119aada0ae1
danxx <danxx@yanhuamedia.com>
2018-03-26 16:10:43 +0800

    添加部分注释

--- sample_data/src/main/java/sample_data/cache/CacheProviders.java
+++ sample_data/src/main/java/sample_data/cache/CacheProviders.java
@@ -15,8 +15,20 @@ import sample_data.entities.User;
  * Created by victor on 04/01/16.
  */
 public interface CacheProviders {
+    /**
+     *  使用Reply包装的原因:
+     */
 
-    @LifeCache(duration = 2, timeUnit = TimeUnit.MINUTES) Observable<Reply<List<Repo>>> getRepos(Observable<List<Repo>> oRepos, DynamicKey userName, EvictDynamicKey evictDynamicKey);
+    /***
+     * danxx
+     * 大概的原理就是传入一个 rxjava 和 retrofit组成的网络Observable 通过RxCache内部的操作 完成网络数据缓存刷新等
+     * @param oRepos rxjava 和 retrofit组成的网络Observable
+     * @param userName DynamicKey
+     * @param evictDynamicKey EvictDynamicKey
+     * @return
+     */
+    @LifeCache(duration = 2, timeUnit = TimeUnit.MINUTES)
+    Observable<Reply<List<Repo>>> getRepos(Observable<List<Repo>> oRepos, DynamicKey userName, EvictDynamicKey evictDynamicKey);
 
     @LifeCache(duration = 2, timeUnit = TimeUnit.MINUTES)
     Observable<Reply<List<User>>> getUsers(Observable<List<User>> oUsers, DynamicKey idLastUserQueried, EvictProvider evictProvider);
